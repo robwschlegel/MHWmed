81a0f1b6598381e91f5565f031e57bb1dbc06bd6
robwschlegel <robwschlegel@gmail.com>
2020-12-22 10:41:29 +0100

    Starting on the SST download script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MHWmed
+
+The code used to detect MHWs in SST/in situ data for the Mediterranean. 
---
+++ code/SST_download.R
@@ -0,0 +1,23 @@
+# SST_download.R
+# This script contains the code to FTP access the hi-res SST product for the Med
+
+
+# Setup -------------------------------------------------------------------
+
+# Packages not available via CRAN
+remotes::install_github("skgrange/threadr")
+
+# The packages we will use
+library(tidyverse) # A staple for most modern data management in R
+# library(RCurl) # For helping R to make sense of URLs for web hosted data
+# library(XML) # For reading the HTML tables created by RCurl
+library(tidync) # For easily dealing with NetCDF data
+# library(doParallel) # For parallel processing
+library(threadr) # For downloading from FTP sites that require user credentials
+# library(RCMEMS) # For subsetting CMEMS data before download
+
+
+# Download ----------------------------------------------------------------
+
+# ftp://nrt.cmems-du.eu/Core/
+
