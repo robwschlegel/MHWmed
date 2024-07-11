ed8afcb846af15e56a5832725f7de31611a73c43
Robert Schlegel <robert@ecomonitor.info>
2024-07-11 11:08:12 +0200

    Updated SST downloading script

--- code/SST_download.R
+++ code/SST_download.R
@@ -1,32 +1,28 @@
 # RCMEMS
 # The purpose of this script is to download data from the CMEMS portal
 # This data portal is designed to work with python
-# So gettting it to run with R is a bit of a hack, but it is supported
+# So getting it to run with R is a bit of a hack, but it is supported
 
 
 # Setup -------------------------------------------------------------------
 
-# Get the development version of reticulate
-remotes::install_github('rstudio/reticulate')
+# This script does not call any packages explicitly
+# But does require that some be installed, as may be seen below
 
-# The packages we will use
-library(tidyverse) # A staple for most modern data management in R
-library(tidync) # For easily dealing with NetCDF data
-library(reticulate) # For using Python in R
+# Install Copernicus marine toolbox directly via python via the terminal
+## NB: This requires that python and pip are installed locally
+# system("python -m pip install copernicusmarine")
 
-# NB: Another way to do this, but not recommended. Easier to use CLI
-# Set up virtual environment
-# virtualenv_create(envname = "CopernicusMarine", python = install_python())
-# virtualenv_install("CopernicusMarine", packages = c("copernicusmarine"))
-# reticulate::use_virtualenv("CopernicusMarine", required = TRUE)
-# Store the python package to use the functions in R
-# cmt <- import("copernicusmarine")
-# Add login crednetials if necessary
-# I.e. https://www.copernicus.eu/en
-# cmt$login("<username>", "<password>")
+# Load credentials
+## NB: This file is not uploaded to GitHub to avoid security breach
+## Create account here: https://marine.copernicus.eu/
+# And then uncomment and run this code:
+# CMEMS_cred <- data.frame(username = "username", password = "password")
+# readr::write_csv(CMEMS_cred, "metadata/CMEMS_cred.csv")
+CMEMS_cred <- readr::read_csv("metadata/CMEMS_cred.csv")
 
-# Install copernicus marine toolbox directly via python via the terminal
-system("python -m pip install copernicusmarine")
+# NB: This stopped working on RStudio, but works on Positron...
+# I assume it's something to do with not being able to fins the python path correctly in RStudio
 
 
 # Download ----------------------------------------------------------------
@@ -35,23 +31,31 @@ system("python -m pip install copernicusmarine")
 subset_CMEMS <- function(df, data_ID, var_ID, out_dir){
   start_date <- df$start_date; end_date <- df$end_date
   out_name <- paste0(data_ID,"_",start_date,"_", end_date)
-  system(paste("copernicusmarine subset -i", data_ID, "--start-datetime", start_date, "--end-datetime", end_date, 
-              "-v", var_ID, "-o", out_dir, "-f", out_name, "--force-download"))
+  if(!file.exists(paste0(out_dir, out_name))){
+    system(paste("copernicusmarine subset -i", data_ID,
+    "--username", CMEMS_cred$username, "--password", CMEMS_cred$password,
+    "--start-datetime", start_date, "--end-datetime", end_date, 
+    "-v", var_ID, "-o", out_dir, "-f", out_name, "--force-download"))
+  }
+
 }
 
 # Dataframe of start and end dates to cycle through
-dates <- data.frame(start_date = paste0(1982:2023, "-01-01"), end_date = paste0(1982:2023, "-12-31")) |> 
-  mutate(row_idx = 1:n())
+dates <- data.frame(start_date = paste0(1982:2023, "-01-01"), 
+                    end_date = paste0(1982:2023, "-12-31"),
+                    row_idx = seq_len(length(1982:2023)))
 
 # Ply the downloads by year
 ## Takes about 20 seconds per year
-plyr::d_ply(dates, c("row_idx"), subset_CMEMS,
+plyr::d_ply(dates, c("row_idx"), subset_CMEMS, .parallel = FALSE,
             data_ID = "cmems_SST_MED_SST_L4_REP_OBSERVATIONS_010_021", 
-            var_ID = "analysed_sst", out_dir = "~/data/CMEMS_Med/")
+            var_ID = "analysed_sst", out_dir = "~/data/MED_REP/")
 
 
 # Test --------------------------------------------------------------------
 
-ncdf4::nc_open("~/data/CMEMS_Med/cmems_SST_MED_SST_L4_REP_OBSERVATIONS_010_021_1982-01-01_1982-12-31.nc")
+ncdf4::nc_open("~/data/MED_REP/cmems_SST_MED_SST_L4_REP_OBSERVATIONS_010_021_2023-01-01_2023-12-31.nc")
 
-sst_test <- tidync::tidync("~/data/CMEMS_Med/cmems_SST_MED_SST_L4_REP_OBSERVATIONS_010_021_1982-01-01_1982-12-31.nc") |> hyper_tibble()
+sst_test <- tidync::tidync("~/data/MED_REP/cmems_SST_MED_SST_L4_REP_OBSERVATIONS_010_021_2023-01-01_2023-12-31.nc") |> 
+  tidync::hyper_tibble()
+
